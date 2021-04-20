5eb173e9beb02299b44811509ee07f1bb9d967e3
SteveJobser <tima_tleg@mail.ru>
2021-04-20 17:07:05 +0600

    Initial commit

---
+++ src/main/java/kz/tlegenov/springboot/springbootrest/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package kz.tlegenov.springboot.springbootrest.dao;
+
+
+
+import kz.tlegenov.springboot.springbootrest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
---
+++ src/main/java/kz/tlegenov/springboot/springbootrest/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package kz.tlegenov.springboot.springbootrest.service;
+
+
+
+import kz.tlegenov.springboot.springbootrest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
