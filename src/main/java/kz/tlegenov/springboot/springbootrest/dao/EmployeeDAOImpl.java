4237a336b1f509fd226930fa37ee610739a37ad4
SteveJobser <tima_tleg@mail.ru>
2021-04-21 23:48:27 +0600

    upcast to JPA from Hibernate interfaces

--- src/main/java/kz/tlegenov/springboot/springbootrest/dao/EmployeeDAOImpl.java
+++ src/main/java/kz/tlegenov/springboot/springbootrest/dao/EmployeeDAOImpl.java
@@ -1,13 +1,14 @@
 package kz.tlegenov.springboot.springbootrest.dao;
 
 import kz.tlegenov.springboot.springbootrest.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -19,30 +20,46 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public List<Employee> getAllEmployees() {
 
-        Session session = entityManager.unwrap(Session.class);
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
-        List<Employee> allEmployees = query.getResultList();
+        // old Hibernate method
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
 
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> allEmployees = query.getResultList();
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
-//        Session session = sessionFactory.getCurrentSession();
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        // old Hibernate method
+//        Session session = entityManager.unwrap(Session.class);
 //        session.saveOrUpdate(employee);
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
+
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+        // old Hibernate method
+//        Session session = entityManager.unwrap(Session.class);
 //        Employee employee = session.get(Employee.class, id);
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+//        Session session = entityManager.unwrap(Session.class);
 //        Employee employee = session.get(Employee.class, id);
 //        session.delete(employee);
-//    }
+
+        Query query = entityManager.createQuery("delete from Employee " + "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
